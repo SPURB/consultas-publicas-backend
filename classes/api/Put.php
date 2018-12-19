5337b8cc0bc4374caa418a4c1b6b010b60d1f327
hsolucas <hsolucas@yahoo.com.br>
2018-12-19 16:34:08 -0200

    Ajuste exceptions #14

--- classes/api/Put.php
+++ classes/api/Put.php
@@ -11,50 +11,47 @@ class Put extends APIMethod{
 		$input = json_decode(file_get_contents('php://input'),true);
 
 		$result = NULL;
-		try{
-			if($id <= 0){
-				if(!is_array($input) || count($input) <= 0){
-					throw new APIException("$id nao encontrado", 400);
-				}
-				$updated = 0;
-				foreach ($input as $item) {
-					$obj = APIMethod::getTable($table);
-					$pkName = $obj->getPKColName();
-					if(!isset($item[$pkName])){
-						throw new APIException("Objeto a atualizar fornecido sem ID: $pkName", 400);
-					}
 
-					$id = $item[$pkName];
-					$objOrig = $obj->obter($id);
-					$up = intval(Put::updateObject($obj, $objOrig, $item));
-					if($up == 0){
-						throw new APIException("Nao atualizado com ID $id", 500);
-					}
-					$updated++;
-				}
-
-
-				$result = $updated;
-			}else{
+		if($id <= 0){
+			if(!is_array($input) || count($input) <= 0){
+				throw new APIException("$id nao encontrado", 400);
+			}
+			$updated = 0;
+			foreach ($input as $item) {
 				$obj = APIMethod::getTable($table);
-				$objOrig = $obj->obter($id);
-				if($objOrig === FALSE){
-					throw new APIException("$id nao encontrado", 404);
+				$pkName = $obj->getPKColName();
+				if(!isset($item[$pkName])){
+					throw new APIException("Objeto a atualizar fornecido sem ID: $pkName", 400);
 				}
-				$result = Put::updateObject($obj, $objOrig, $input);
+
+				$id = $item[$pkName];
+				$objOrig = $obj->obter($id);
+				$up = intval(Put::updateObject($obj, $objOrig, $item));
+				if($up == 0){
+					throw new APIException("Nao atualizado com ID $id", 500);
+				}
+				$updated++;
 			}
 
-			if(!$result || $result == 0){
-				$msg = "";
-				foreach($obj as $key => $val){
-					$msg.=" ".$key." | ".$val;
-				}
-				throw new APIException("$msg Erro na atualização.", 500);
+
+			$result = $updated;
+		}else{
+			$obj = APIMethod::getTable($table);
+			$objOrig = $obj->obter($id);
+			if($objOrig === FALSE || !is_object($objOrig)){
+				throw new APIException("$id nao encontrado", 404);
 			}
-		}catch(Exception $ex){
-			http_response_code($ex->getCode());
-			$result=$ex->getMessage();
+			$result = Put::updateObject($obj, $objOrig, $input);
 		}
+
+		if(!$result || $result == 0){
+			$msg = "";
+			foreach($obj as $key => $val){
+				$msg.=" ".$key." | ".$val;
+			}
+			throw new APIException("$msg Erro na atualização.", 500);
+		}
+		
 		return $result;
 
 	}
